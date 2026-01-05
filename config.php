17aed02b2be9ccb45627407c6a9321cab40e84d5
Mariia Ingersoll <maria.nagornaya1996@gmail.com>
2026-01-05 20:08:34 +0200

    Настройка куки

--- config.php
+++ config.php
@@ -3,6 +3,13 @@
 ini_set('session.use_only_cookies', 1); // Использовать ТОЛЬКО куки для хранения ID сессии. 1 - значит true
 ini_set('session.use_strict_mode', 1); //PHP будет проверять, что ID сессии существует и действителен. Нельзя придумать свой ID
 
+session_set_cookie_params([
+    'lifetime' => 1800,
+    'domain' => 'localhost',
+    'path' => '/',
+    'secure' => false,
+    'httponly' => true,
+]);
 
 session_start();
 
