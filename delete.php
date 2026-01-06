6e88e21402828c2faa715c0e2190573ce7438d67
Mariia Ingersoll <maria.nagornaya1996@gmail.com>
2026-01-06 17:10:24 +0200

    Стилизация кнопки "очистить все"

---
+++ delete.php
@@ -0,0 +1,10 @@
+<?php
+// require_once 'config.php';
+
+// echo "<ol>";
+// foreach($_SESSION['habits'] as $habit) {
+//     echo "<li> Название привычки: " . $habit['name'] . "создана в " . $habit['created'] . " </li>";
+//     echo "<br>";
+// }
+
+// echo "</ol>";
--- index.php
+++ index.php
@@ -74,16 +74,21 @@ require_once 'config.php';
                     <p>Добавьте первую привычку выше</p>
                 </div>
                 <?php else: ?>
-                    <ul class="habits-list">
                         <?php foreach($_SESSION['habits'] as $habit): ?>
-                                <li class='habit-item'>
+                            <div class="habit-name" >
                                 <?php echo htmlspecialchars($habit['name']); ?>
-                                </li>
-                <?php endforeach; ?>
+                            </div>
+                        <?php endforeach; ?>
             <?php endif; ?>
 
-
-        <button class='btn-clear'> Очистить все</button>
+            <!-- Кнопка очистки -->
+            <div class="clear-section">
+                <form method="POST" action="clear.php" onsubmit="return confirm('Удалить ВСЕ привычки?')">
+                    <button type="submit" class="btn btn-red">
+                        🗑️ Очистить все
+                    </button>
+                </form>
+            </div>
 
         <footer style="margin-top: 40px; text-align: center; color: #777; font-size: 14px;">
             <p>Простой трекер привычек на день</p>
